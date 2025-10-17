7f004ebbbfb7023ab5f309d699d9ccf5ba2f553b
Justine Chollo Catanghal <chollocatanghal@gmail.com>
2025-10-18 00:15:49 +0800

    Refactor User class with getters, setters, and display method

--- User.java
+++ User.java
@@ -1,10 +1,12 @@
+/**
+ * Superclass representing a generic user (student or tutor).
+ */
 public class User {
     private int userTutoredID;
     private String nameUser;
     private String roleUser;
     private String emailUser;
 
-    //Constructor to create a user
     public User(int userTutoredID, String nameUser, String roleUser, String emailUser) {
         this.userTutoredID = userTutoredID;
         this.nameUser = nameUser;
@@ -12,9 +14,39 @@ public class User {
         this.emailUser = emailUser;
     }
 
-    //Display Info
-    public void displayInfo() {
-        System.out.println("User ID: " + userTutoredID + "\nUsername: " + nameUser + "\nRole: " + roleUser + "\nEmail: " + emailUser);
+    public int getUserTutoredID() {
+        return userTutoredID;
     }
 
+    public void setUserTutoredID(int userTutoredID) {
+        this.userTutoredID = userTutoredID;
+    }
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+
+    public String getRoleUser() {
+        return roleUser;
+    }
+
+    public void setRoleUser(String roleUser) {
+        this.roleUser = roleUser;
+    }
+
+    public String getEmailUser() {
+        return emailUser;
+    }
+
+    public void setEmailUser(String emailUser) {
+        this.emailUser = emailUser;
+    }
+
+    public void displayInfo() {
+        System.out.println("ID: " + userTutoredID + ", Name: " + nameUser + ", Role: " + roleUser + ", Email: " + emailUser);
+    }
 }
